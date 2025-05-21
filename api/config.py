55b2496ad2fb6fa4fd94f895c1cc36be42a300c1
Ramya B L <ramyabl@togglr.com>
2025-05-21 09:49:54 +0000

    config changes

--- api/config.py
+++ api/config.py
@@ -2,5 +2,5 @@ from configparser import ConfigParser
 
 def get_db_config():
     cp = ConfigParser()
-    cp.read('../config/config.ini')
+    cp.read('/home/ramya/Ramya_project/flask-airflow-etl/config/config.ini')
     return dict(cp['postgres'])
